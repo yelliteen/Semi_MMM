6357133b88ca0e48cbc60e2b1a82944923ddbe29
Park.T.G <xorb5693@naver.com>
2020-05-12 17:21:57 +0900

    TG Change Commit

--- Server/semi_mmm/src/admin/controller/AdminServlet.java
+++ Server/semi_mmm/src/admin/controller/AdminServlet.java
@@ -27,7 +27,7 @@ public class AdminServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		request.getRequestDispatcher("/WEB-INF/admin/adminIndex.jsp").forward(request, response);
+		request.getRequestDispatcher("/WEB-INF/views/admin/adminIndex.jsp").forward(request, response);
 	}
 
 	/**
