feb91a14390bc2a6c432eee515adf33b1cc9aa41
KkimBang <60415103+KkimBang@users.noreply.github.com>
2020-05-14 10:06:22 +0900

    commit

--- Server/semi_mmm/src/member/controller/JoinFrm2Servlet.java
+++ Server/semi_mmm/src/member/controller/JoinFrm2Servlet.java
@@ -8,16 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class JoinFrmServlet
+ * Servlet implementation class JoinFrm2Servlet
  */
-@WebServlet(name = "JoinFrm", urlPatterns = { "/joinFrm" })
-public class JoinFrmServlet extends HttpServlet {
+@WebServlet(name = "JoinFrm2", urlPatterns = { "/joinFrm2" })
+public class JoinFrm2Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public JoinFrmServlet() {
+    public JoinFrm2Servlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -26,7 +26,7 @@ public class JoinFrmServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/WEB-INF/views/member/joinFrm.jsp").forward(request, response);
+		request.getRequestDispatcher("/WEB-INF/views/member/joinFrm2.jsp").forward(request, response);
 	}
 
 	/**
