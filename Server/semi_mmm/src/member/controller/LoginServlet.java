929a6afb0a1c71d90733792b40677049d0ab25f9
Park.T.G <xorb5693@naver.com>
2020-05-21 10:44:18 +0900

    tg commit

--- Server/semi_mmm/src/member/controller/LoginServlet.java
+++ Server/semi_mmm/src/member/controller/LoginServlet.java
@@ -1,9 +1,7 @@
 package member.controller;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,10 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.google.gson.Gson;
-
-import dog.model.service.DogService;
-import dog.model.vo.Dog;
 import member.model.service.MemberService;
 import member.model.vo.Member;
 
